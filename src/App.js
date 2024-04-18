e63b4e20db304b7ae64c91419244d99808dcc3fa
Zeraphim <diamantejc87@gmail.com>
2024-04-18 10:59:29 +0800

    added react toast

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Error from "./components/pages/error/Error";
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
 
--- src/components/pages/states/States.js
+++ src/components/pages/states/States.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 // importing the useState hook
 import { useState } from "react";
 
@@ -9,16 +12,39 @@ function States() {
   // using the useState hook to create a state variable for user chosen color
   const [color, setColor] = useState("white");
 
+  const notify = () => toast.success('🦄 Wow so easy!', {
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark",
+  });
+
   return (
     <div className="content" style={{ backgroundColor: color }}>
-      
 
+      <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      />
+    
       {/* <h2>Change the bg color using React States</h2> */}
       <h2>THIS IS A NEW TEXT 🤣</h2>
       <h3>Current Color: {color}</h3>
 
       <div className="button-container">
-        <button onClick={() => setColor("white")}>White</button>
+        <button onClick={notify}>White</button> {/*  () => setColor("white") */}
         <button onClick={() => setColor("yellow")}>Yellow</button>
         <button onClick={() => setColor("cyan")}>Cyan</button>
         <button onClick={() => setColor("teal")}>Teal</button>
